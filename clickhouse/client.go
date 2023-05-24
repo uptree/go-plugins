2f60a78d313f1be67d98f9008aec5a75572a8953
mark <uptree@qq.com>
2023-05-24 10:35:31 +0800

    clickhouse-client

---
+++ clickhouse/client.go
@@ -0,0 +1,43 @@
+package clickhouse
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/mailru/dbr"
+	_ "github.com/mailru/go-clickhouse"
+)
+
+// ClientConfig ...
+type ClientConfig struct {
+	Host            string
+	Port            int
+	User            string
+	Password        string
+	DBName          string
+	MaxOpenConns    int
+	MaxIdleConns    int
+	ConnMaxLifetime int
+	ConnMaxIdleTime int
+}
+
+// NewClient ...
+func NewClient(config *ClientConfig) *sql.DB {
+	dsn := fmt.Sprintf("http://%s:%s@%s:%d/%s",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.DBName,
+	)
+	conn, err := dbr.Open("clickhouse", dsn, nil)
+	if err != nil {
+		panic(err)
+	}
+	conn.SetMaxIdleConns(config.MaxIdleConns)
+	conn.SetMaxOpenConns(config.MaxOpenConns)
+	conn.SetConnMaxLifetime(time.Duration(config.ConnMaxLifetime) * time.Minute)
+	conn.SetConnMaxIdleTime(time.Duration(config.ConnMaxIdleTime) * time.Minute)
+	return conn.DB
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/uptree/go-plugins
 go 1.15
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/json-iterator/go v1.1.12
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mailru/dbr v3.0.0+incompatible
+	github.com/mailru/go-clickhouse v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/spf13/viper v1.15.0
 	golang.org/x/text v0.5.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
 )
